5d0cd9c2b2061ce1c9b7be48dcc0cd3d6c98b828
Bumblebee567 <maciekgasiorek9@gmail.com>
2018-03-13 00:21:23 +0100

    Starting parser application from client

--- RSSReader/MainWindow.xaml.cs
+++ RSSReader/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -21,13 +22,33 @@ namespace RSSReader
     /// </summary>
     public partial class MainWindow : Window
     {
+        static Process process;
         public MainWindow()
         {
             InitializeComponent();
             Utiles.AddChannelsToCombobox(ChannelsList);
-            //Utiles.AddFeedsToListbox("Najnowsze", FeedList);
-            var context = new RSSFeedDatabaseEntities();
-            FeedList.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("FeedID", System.ComponentModel.ListSortDirection.Descending));
+
+            var startInfo = new ProcessStartInfo();
+            startInfo.WorkingDirectory = "C:/RSSParser";
+            startInfo.FileName = "Parser.exe";
+            process = Process.Start(startInfo);
+
+            Timer tmr = new Timer();
+            tmr.Elapsed += Tmr_Elapsed;
+            tmr.Interval = 300000;
+            tmr.Start();
+
+            Application.Current.MainWindow.Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            process.Kill();
+        }
+
+        private void Tmr_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            MessageBox.Show("Nowe wiadomości zostały pobrane, wybierz ponownie kategorię", "Informacja", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
@@ -37,6 +58,7 @@ namespace RSSReader
 
         private void FeedList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            FeedList.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("FeedID", System.ComponentModel.ListSortDirection.Descending));
             ImageCanvas.Children.Clear();
             Feed currentFeed = (Feed)FeedList.Items.CurrentItem;
             if (currentFeed != null)
