94196073d5f0956fbc1f6537b80a2367daa167b9
AdrianS95 <adrian.smaza@interia.pl>
2018-03-19 17:53:27 +0100

    Improvement of tests in customer

--- RSSReader/UnitTests.cs
+++ RSSReader/UnitTests.cs
@@ -20,48 +20,49 @@ namespace RSSReader
                 var items = target.FeedID;
                 Assert.IsNotNull(items);
             }
-
+            [TestCase]
             public void CheckIf_ChannelID_HaveID()
             {
                 var target = new Feed();
                 var items = target.ChannelID;
                 Assert.IsNotNull(items);
             }
-
+            [TestCase]
             public void CheckIf_Title_HaveTitle()
             {
                 var target = new Feed();
                 var items = target.Title;
                 Assert.IsNull(items);
             }
-
+            [TestCase]
             public void CheckIf_Link_HaveLink()
             {
                 var target = new Feed();
                 var items = target.Link;
                 Assert.IsNull(items);
             }
+            [TestCase]
             public void CheckIf_Description_HaveDescription()
             {
                 var target = new Feed();
                 var items = target.Description;
                 Assert.IsNull(items);
             }
-
+            [TestCase]
             public void CheckIf_Imagelink_HaveImmageLink()
             {
                 var target = new Feed();
                 var items = target.Imagelink;
                 Assert.IsNull(items);
             }
-
+            [TestCase]
             public void CheckIf_Date_HaveDate()
             {
                 var target = new Feed();
                 var items = target.Date;
                 Assert.IsNull(items);
             }
-
+            [TestCase]
             public void CheckIf_Channel_HaveChanel()
             {
                 var target = new Feed();
