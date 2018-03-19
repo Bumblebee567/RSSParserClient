a3e62c057dba55bd6a972ca1bbfadb7e66b3bbf9
Bumblebee567 <maciekgasiorek9@gmail.com>
2018-03-19 19:27:16 +0100

    Logic exported to class Utiles from code behind

--- RSSReader/MainWindow.xaml.cs
+++ RSSReader/MainWindow.xaml.cs
@@ -12,80 +12,50 @@ namespace RSSReader
     /// </summary>
     public partial class MainWindow : Window
     {
-        public static Process process;
         public static int numOfFeeds;
         public MainWindow()
         {
             InitializeComponent();
             Utiles.AddChannelsToCombobox(ChannelsList);
-
-            var startInfo = new ProcessStartInfo();
-            startInfo.FileName = @"C:\RSSParser\Parser\bin\Debug\Parser.exe";
-            process = Process.Start(startInfo);
-
-            Timer tmr = new Timer();
-            tmr.Elapsed += Tmr_Elapsed;
-            tmr.Interval = 330000;
-            tmr.Start();
-
+            Utiles.StartParser();
+            Application.Current.MainWindow.Closing += MainWindow_Closing;
             using (var context = new RSSFeedDatabaseEntities())
             {
                 numOfFeeds = context.Feed.Count();
             }
-
-            Application.Current.MainWindow.Closing += MainWindow_Closing;
+            Timer tmr = new Timer();
+            tmr.Elapsed += Tmr_Elapsed;
+            tmr.Interval = 330000;
+            tmr.Start();
         }
-
-        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            process.Kill();
-        }
-
         private void Tmr_Elapsed(object sender, ElapsedEventArgs e)
         {
             using (var context = new RSSFeedDatabaseEntities())
             {
-                var newFeedsNumber = context.Feed.Count() - numOfFeeds;
-                if (newFeedsNumber != 0)
+                var newFeedsOnChannel = Utiles.GetNumberOfNewFeedsOnChannel(ChannelsList.Text, numOfFeeds);
+                if (newFeedsOnChannel != 0)
                 {
-                    MessageBox.Show($"Nowe wiadomości ({newFeedsNumber}) zostały pobrane, kliknij \"pokaż\" by odświeżyć", "TVN RSS Reader",
+                    MessageBox.Show($"Nowe wiadomości ({newFeedsOnChannel}) zostały pobrane, kliknij \"pokaż\" by odświeżyć", "TVN RSS Reader",
                         MessageBoxButton.OK, MessageBoxImage.Information);
-                    numOfFeeds = newFeedsNumber;
+                    numOfFeeds = newFeedsOnChannel;
                 }
             }
         }
-
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            var process = Process.GetProcessesByName("Parser").First();
+            process.Kill();
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             Utiles.AddFeedsToListbox(ChannelsList.Text, FeedList);
-            TitleBlock.Text = String.Empty;
-            DateBlock.Text = String.Empty;
-            DescriptionBlock.Text = String.Empty;
-            ImageCanvas.Children.Clear();
+            Utiles.ClearUserControls(TitleBlock, DateBlock, DescriptionBlock, ImageCanvas);
         }
-
         private void FeedList_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            FeedList.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("FeedID", System.ComponentModel.ListSortDirection.Descending));
-            ImageCanvas.Children.Clear();
-            Feed currentFeed = (Feed)FeedList.Items.CurrentItem;
-            if (currentFeed != null)
-            {
-                TitleBlock.Text = currentFeed.Title;
-                if (currentFeed.Description.Length > 400)
-                {
-                    DescriptionBlock.Text = currentFeed.Description.Substring(0, currentFeed.Description.Length - (currentFeed.Description.Length - 300));
-                }
-                else
-                {
-                    DescriptionBlock.Text = currentFeed.Description;
-                }
-                DateBlock.Text = currentFeed.Date;
-                ImageCanvas.Children.Add(Utiles.GetImage(currentFeed.Imagelink));
-                HyperlinkBox.NavigateUri = new Uri(currentFeed.Link);
-            }
+            Utiles.SortFeedsInList(FeedList);
+            Utiles.GetDetailsOfSelectedFeed(FeedList, TitleBlock, DateBlock, DescriptionBlock, ImageCanvas, HyperlinkBox);
         }
-
         private void HyperlinkBox_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e)
         {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
--- RSSReader/Utiles.cs
+++ RSSReader/Utiles.cs
@@ -1,16 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Controls;
+using System.Windows.Documents;
 using System.Windows.Media.Imaging;
 
 namespace RSSReader
 {
     class Utiles
     {
-        public static Image GetImage (string imageUrl)
+        public static void StartParser()
+        {
+            var startInfo = new ProcessStartInfo();
+            startInfo.FileName = @"C:\RSSParser\Parser\bin\Debug\Parser.exe";
+            Process.Start(startInfo);
+        }
+        public static Image GetImage(string imageUrl)
         {
             if (imageUrl == "brak zdjęcia")
             {
@@ -45,6 +53,44 @@ namespace RSSReader
             {
                 feedsOnChannel = context.Feed.Where(x => x.Channel.Title == channelTitle);
                 feedListBox.ItemsSource = feedsOnChannel.ToList();
+            }
+        }
+        public static void ClearUserControls(TextBlock title, TextBlock date, TextBlock description, Canvas image)
+        {
+            title.Text = String.Empty;
+            date.Text = String.Empty;
+            description.Text = String.Empty;
+            image.Children.Clear();
+        }
+        public static void SortFeedsInList(ListBox feedList)
+        {
+            feedList.Items.SortDescriptions.Add(new System.ComponentModel.SortDescription("FeedID", System.ComponentModel.ListSortDirection.Descending));
+        }
+        public static void GetDetailsOfSelectedFeed(ListBox feeds, TextBlock title, TextBlock date, TextBlock description, Canvas image, Hyperlink link)
+        {
+            image.Children.Clear();
+            Feed currentFeed = (Feed)feeds.Items.CurrentItem;
+            if (currentFeed != null)
+            {
+                title.Text = currentFeed.Title;
+                if (currentFeed.Description.Length > 400)
+                {
+                    description.Text = currentFeed.Description.Substring(0, currentFeed.Description.Length - (currentFeed.Description.Length - 300));
+                }
+                else
+                {
+                    description.Text = currentFeed.Description;
+                }
+                date.Text = currentFeed.Date;
+                image.Children.Add(Utiles.GetImage(currentFeed.Imagelink));
+                link.NavigateUri = new Uri(currentFeed.Link);
+            }
+        }
+        public static int GetNumberOfNewFeedsOnChannel(string channelTitle, int numOfFeeds)
+        {
+            using (var context = new RSSFeedDatabaseEntities())
+            {
+                return context.Feed.Where(x => x.Channel.Title == channelTitle).Count() - numOfFeeds;
             }
         }
     }
